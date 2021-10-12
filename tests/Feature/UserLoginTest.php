3ada9b82c2a98a5aa48ac03336e575527d492830
miyunecadz <jvcadz@gmail.com>
2021-10-12 18:47:30 +0800

    Add additional test to user Login test

--- tests/Feature/UserLoginTest.php
+++ tests/Feature/UserLoginTest.php
@@ -25,4 +25,50 @@ class UserLoginTest extends TestCase
 
         $response->assertRedirect(route('dashboard'));
     }
+
+    /**
+     * @test
+     */
+    public function userLoginUsernameShouldBeRequired()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->post(route('login'),[
+            'password' => '1234'
+        ]);
+
+        $response->assertRedirect(route('login-page'));
+        $response->assertSessionHasErrors(['username']);
+    }
+
+    /**
+     * @test
+     */
+    public function userLoginPasswordShouldBeRequired()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->post(route('login'),[
+            'username' => 'admin'
+        ]);
+
+        $response->assertRedirect(route('login-page'));
+        $response->assertSessionHasErrors(['password']);
+    }
+
+    /**
+     * @test
+     */
+    public function userLoginReturnErrorIfCredentialsAreInvalid()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->post(route('login'),[
+            'username' => 'sample',
+            'password' => '1234567890'
+        ]);
+
+        $response->assertRedirect(route('login-page'));
+        $response->assertSessionHasErrors(['success' => false, 'message']);
+    }
 }
