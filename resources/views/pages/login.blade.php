06b91c21ee7859047701037f5692b8eed91b455a
Mujina <m4rc42@yahoo.com>
2021-10-18 12:33:36 +0800

    update login and add css fonts

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -1,57 +1,42 @@
 @extends('layout.app')
 
 @section('content')
-    <!-- Root element for center items -->
-    <div class="flex flex-col h-screen  bg-gray-100">
-        <!-- Auth Card Container -->
+    <div class="container-login min-vh-100">
+        <div class="container">
+            <div class="row px-3">
+                <div class="col-lg-10 offset-lg-1 my-3 border rounded-3 vh-100 bg-white">
+                    <div class="row h-100">
+                        <div class="col-md-6 d-none d-md-inline bg-image">
 
-        <div class="grid place-items-center mx-2 my-20 sm:my-auto">
-            <!-- Auth Card -->
-            <div class="  w-11/12 p-12 sm:w-8/12 md:w-6/12 lg:w-5/12 2xl:w-4/12
-    px-6 py-10 sm:px-10 sm:py-6
-    bg-white rounded-lg shadow-md lg:shadow-lg">
+                        </div>
+                        <div class="col-md-6 col-sm-12 ">
+                            <div class="flex-row d-flex  justify-content-center align-items-center h-100">
+                                <div class="col-12">
+                                    <h4 class="d-flex justify-content-center font-montserrat-bold mb-4">Welcome back</h4>
+                                    <form  action="" class="p-4">
+                                        <div class="input-icons">
+                                            <i class="bi bi-person-fill icon"></i>
+                                            <input type="text" name="username" class="input-field font-poppins-regular border-0 rounded-pill" placeholder="Username">
+                                        </div>
+                                        <div class="input-icons">
+                                            <i class="bi bi-person-fill icon"></i>
+                                            <input type="password" name="password" class="input-field font-poppins-regular border-0 rounded-pill" placeholder="Password">
+                                        </div>
 
-                <!-- Card Title -->
-                <h2 class=" text-center font-semibold text-3xl lg:text-4xl text-gray-800
-    my-5 pb-5">
-                    ALS Management System
-                </h2>
-                @error('message')
-                    <div class="text-red-600 text-xs my-3">
-                        {{ $message }}
+                                        <div class="d-grid">
+                                            <button type="submit" class="btn btn-success font-poppins-regular  rounded-pill">
+                                                Login
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+
+
+                            </div>
+                        </div>
                     </div>
-                @enderror
 
-                <form class="mt-10" action="{{route('login')}}" method="POST">
-                    @csrf
-                    <!-- Email Input -->
-                    <label for="username" class="block text-xs font-semibold text-gray-600 uppercase">Username</label>
-                    <input id="username" value="{{old('username')}}" type="text" name="username" placeholder="JuanDelaCruz" autocomplete="user"
-                            class="block w-full py-3 px-1 mt-2
-            text-gray-800 appearance-none
-            border-b-2 border-gray-100
-            focus:text-gray-500 focus:outline-none focus:border-gray-200"
-                            />
-
-
-
-                    <!-- Password Input -->
-                    <label for="password" class="block mt-2 text-xs font-semibold text-gray-600 uppercase">Password</label>
-                    <input id="password" type="password" name="password" placeholder="***********" autocomplete="current-password"
-                            class="block w-full py-3 px-1 mt-2 mb-4
-            text-gray-800 appearance-none
-            border-b-2 border-gray-100
-            focus:text-gray-500 focus:outline-none focus:border-gray-200"
-                            />
-
-                    <!-- Auth Buttton -->
-                    <button type="submit"
-                            class="w-full py-3 mt-10 bg-gray-800 rounded-sm
-            font-medium text-white uppercase
-            focus:outline-none hover:bg-gray-700 hover:shadow-none">
-                        Login
-                    </button>
-                </form>
+                </div>
             </div>
         </div>
     </div>
