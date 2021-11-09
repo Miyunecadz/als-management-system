4dab8069a111c06c13ce577341420653a81889c5
Mujina <m4rc42@yahoo.com>
2021-11-09 12:38:51 +0800

    added edit profile

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,20 +1,36 @@
 <nav class="nav-height bg-green-light-flat border-bottom border-success shadow-sm
-            d-flex flex-row justify-content-between w-100 pt-1 overflow-hidden">
+            d-flex flex-row justify-content-between w-100 pt-1 ">
     <div>
         <div class="d-inline d-sm-none btn p-0 border-0 mx-2" style="font-size:30px" onclick="toggleSidebar()">
             <i class="bi bi-list"></i>
         </div>
     </div>
 
-    <div class="font-poppins-regular float-right">
-        <a href="#" class="text-decoration-none nav-height
-                            d-flex align-items-center
-                                px-3 text-white">
-                <span>
-                    <i class="bi bi-door-open-fill"></i>
-                    Logout
-                </span>
-        </a>
+    <div class="font-poppins-regular float-right ">
+        <div class="dropdown py-2 px-3">
+            <span
+                class="rounded-circle dropdown-toggle user-icon"
+                id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                Juan Dela Cruz
+            </span>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+
+                <li>
+                    <a class="dropdown-item" href="#">
+                        <i class="bi bi-person-circle"></i>
+                         Edit Profile
+                    </a>
+                </li>
+                <li>
+                    <a class="dropdown-item" href="#">
+                        <i class="bi bi-box-arrow-right"></i>
+                         Log-out
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+
     </div>
 
 {{--    <form action="" method="GET" class="mx-2">--}}
