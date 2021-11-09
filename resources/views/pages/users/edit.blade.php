f7aa74561c28dd8c9df94bd51ea10ec672c08372
Mujina <m4rc42@yahoo.com>
2021-11-09 12:42:12 +0800

    fixed responsive

--- resources/views/pages/users/edit.blade.php
+++ resources/views/pages/users/edit.blade.php
@@ -3,8 +3,8 @@
 @section('contents')
     <h3 class="text-center  my-3">Edit Profile</h3>
 
-    <form action="#" method="POST" class="d-flex justify-content-center">
-        <div class="col-md-4 mb-3 d-flex justify-content-center flex-column">
+    <form action="#" method="POST" class="d-md-flex justify-content-center p-2">
+        <div class="col-md-4 col-12 mb-3 d-md-flex justify-content-center flex-column">
             <div class="form-floating ">
                 <input type="text" name="fullname" class="form-control mb-3" id="fullname" placeholder="Martin" readonly>
                 <label for="fullname">Full Name</label>
@@ -22,7 +22,7 @@
                 <label for="newpass">New Password</label>
             </div>
 
-            <input  type="submit" class="btn btn-primary col-md-7" value="Update">
+            <input  type="submit" class="btn btn-primary col-6 col-md-7" value="Update">
         </div>
     </form>
 @endsection
