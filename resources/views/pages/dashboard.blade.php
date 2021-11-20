5cb7550d96a4784151c1c27bff60389f2cf98480
Mujina <m4rc42@yahoo.com>
2021-11-20 21:26:24 +0800

    fixed role check

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -2,7 +2,7 @@
 
 @section('contents')
     @includeWhen(auth()->check() && auth()->user()->isAdmin(),'pages.admin.dashboard')
-    @includeWhen(auth()->check() && auth()->user()->isAdmin() == 'teacher','pages.teacher.dashboard')
+    @includeWhen(auth()->check() && !auth()->user()->isAdmin() == 'teacher','pages.teacher.dashboard')
 @endsection
 
 
